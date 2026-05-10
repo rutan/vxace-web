d2c70315ce3feefb2a8974b6f10794f9084cec87
rutan <ru_shalm@hazimu.com>
2026-05-11 02:11:10 +0900

    deploy playground

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH ?? '/',
   build: {
     license: {
       fileName: 'license.md',
